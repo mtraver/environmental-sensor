03a0461a2430b853a13c12783cc852f1a9784bc7
Michael Traver <michael.r.traver@gmail.com>
2018-04-02 21:42:23 -0700

    Add tests to measurement package
    Coverage is only 40.9%, but that's an improvement over 0.0%

---
+++ receiver/measurement/measurement_test.go
@@ -0,0 +1,135 @@
+package measurement
+
+import (
+	"testing"
+	"time"
+
+	"github.com/golang/protobuf/ptypes"
+)
+
+func TestToStorableMeasurement(t *testing.T) {
+	deviceID := "foo"
+	var temp float32 = 18.5
+
+	goTimestamp := time.Date(2018, time.March, 25, 0, 0, 0, 0, time.UTC)
+	pbTimestamp, _ := ptypes.TimestampProto(goTimestamp)
+	m := Measurement{
+		DeviceId:  deviceID,
+		Timestamp: pbTimestamp,
+		Temp:      temp,
+	}
+
+	s, err := m.ToStorableMeasurement()
+	if err != nil {
+		t.Errorf("Error in ToStorableMeasurement: %v", err)
+		return
+	}
+
+	if s.DeviceId != deviceID {
+		t.Errorf("Incorrect devie ID. Expected %q, got %q", deviceID, s.DeviceId)
+	}
+
+	if s.Timestamp != goTimestamp {
+		t.Errorf("Incorrect timestamp. Expected %v, got %v", goTimestamp,
+			s.Timestamp)
+	}
+
+	if s.Temp != temp {
+		t.Errorf("Incorrect temp. Expected %v, got %v", temp, s.Temp)
+	}
+}
+
+func TestDBKey(t *testing.T) {
+	m := StorableMeasurement{
+		DeviceId:  "foo",
+		Timestamp: time.Date(2018, time.March, 25, 0, 0, 0, 0, time.UTC),
+		Temp:      18.5,
+	}
+
+	expectedKey := "foo#2018-03-25T00:00:00Z"
+	dbKey := m.DBKey()
+	if dbKey != expectedKey {
+		t.Errorf("Incorrect DB key. Expected %q, got %q", expectedKey, dbKey)
+	}
+}
+
+func TestMeasurementMapToJSONEmpty(t *testing.T) {
+	measurements := map[string][]StorableMeasurement{}
+	marshalledJSON, err := MeasurementMapToJSON(measurements)
+	if err != nil {
+		t.Errorf("Error on valid input: %v", err)
+	}
+
+	expected := "null"
+	if string(marshalledJSON) != expected {
+		t.Errorf("Incorrect JSON. Expected %q, got %q", expected,
+			string(marshalledJSON))
+	}
+}
+
+func TestMeasurementMapToJSONNoMeasurements(t *testing.T) {
+	measurements := map[string][]StorableMeasurement{
+		"foo": []StorableMeasurement{},
+	}
+
+	marshalledJSON, err := MeasurementMapToJSON(measurements)
+	if err != nil {
+		t.Errorf("Error on valid input: %v", err)
+	}
+
+	expected := `[{"id":"foo","values":[]}]`
+	if string(marshalledJSON) != expected {
+		t.Errorf("Incorrect JSON. Expected %q, got %q", expected,
+			string(marshalledJSON))
+	}
+}
+
+func TestMeasurementMapToJSON(t *testing.T) {
+	measurements := map[string][]StorableMeasurement{
+		"foo": []StorableMeasurement{
+			StorableMeasurement{
+				DeviceId:  "foo",
+				Timestamp: time.Date(2018, time.March, 25, 0, 0, 0, 0, time.UTC),
+				Temp:      18.5,
+			},
+			StorableMeasurement{
+				DeviceId:  "foo",
+				Timestamp: time.Date(2018, time.March, 26, 0, 0, 0, 0, time.UTC),
+				Temp:      18.5,
+			},
+			StorableMeasurement{
+				DeviceId:  "foo",
+				Timestamp: time.Date(2018, time.March, 27, 0, 0, 0, 0, time.UTC),
+				Temp:      18.5,
+			},
+		},
+		"bar": []StorableMeasurement{
+			StorableMeasurement{
+				DeviceId:  "bar",
+				Timestamp: time.Date(2018, time.March, 25, 17, 0, 0, 0, time.UTC),
+				Temp:      18.5,
+			},
+			StorableMeasurement{
+				DeviceId:  "bar",
+				Timestamp: time.Date(2018, time.March, 26, 17, 0, 0, 0, time.UTC),
+				Temp:      18.5,
+			},
+			StorableMeasurement{
+				DeviceId:  "bar",
+				Timestamp: time.Date(2018, time.March, 27, 17, 0, 0, 0, time.UTC),
+				Temp:      18.5,
+			},
+		},
+	}
+
+	marshalledJSON, err := MeasurementMapToJSON(measurements)
+	if err != nil {
+		t.Errorf("Error on valid input: %v", err)
+	}
+
+	expected := `[{"id":"bar","values":[{"timestamp":1521997200000,"temp":18.5},{"timestamp":1522083600000,"temp":18.5},{"timestamp":1522170000000,"temp":18.5}]},{"id":"foo","values":[{"timestamp":1521936000000,"temp":18.5},{"timestamp":1522022400000,"temp":18.5},{"timestamp":1522108800000,"temp":18.5}]}]`
+	if string(marshalledJSON) != expected {
+		t.Errorf("Incorrect JSON. Expected %q, got %q", expected,
+			string(marshalledJSON))
+	}
+}

1d6c1bdd386595a0ebd7abd6c9075e7404ce64af
Michael Traver <michael.r.traver@gmail.com>
2018-05-02 22:08:47 -0700

    Go client: Add program that reads temp and prints to stdout

---
+++ client_go/cmd/readtemp/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/mtraver/environmental-sensor/client_go/mcp9808"
+)
+
+func fatal(format string, a ...interface{}) {
+	fmt.Printf(format+"\n", a...)
+	os.Exit(1)
+}
+
+func main() {
+	sensor, err := mcp9808.NewMCP9808()
+	if err != nil {
+		fatal("Error connecting to sensor: %v", err)
+	}
+	defer sensor.Close()
+
+	if err = sensor.Check(); err != nil {
+		fatal("Sensor check failed: %v", err)
+	}
+
+	temp, err := sensor.ReadTemp()
+	if err != nil {
+		fatal("Failed to read temp: %v", err)
+	}
+
+	fmt.Println(temp)
+}

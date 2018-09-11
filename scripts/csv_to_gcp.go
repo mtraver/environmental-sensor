6e9276f2e3f593ce4d2661354956c8b096107334
Michael Traver <michael.r.traver@gmail.com>
2018-09-10 19:36:13 -0700

    gofmt

--- scripts/csv_to_gcp.go
+++ scripts/csv_to_gcp.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-  "bufio"
-  "encoding/csv"
-  "errors"
-  "fmt"
-  "io"
-  "os"
-  "path"
-  "strconv"
-  "time"
+	"bufio"
+	"encoding/csv"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"path"
+	"strconv"
+	"time"
 
-  "golang.org/x/net/context"
+	"golang.org/x/net/context"
 
-  "cloud.google.com/go/pubsub"
-  "github.com/golang/protobuf/proto"
-  "github.com/golang/protobuf/ptypes"
+	"cloud.google.com/go/pubsub"
+	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/ptypes"
 
-  "receiver/measurement"
+	"receiver/measurement"
 )
 
 const usageStr = `usage: %v csv_file project_id topic_id device_id
@@ -54,129 +54,129 @@ Arguments:
 const timeFormat = "2006-01-02T15:04:05.999999"
 
 func strsToFloats(x []string) ([]float32, error) {
-  var numbers []float32
-  for _, v := range x {
-    f, err := strconv.ParseFloat(v, 32)
-    if err != nil {
-      return numbers, err
-    }
-    numbers = append(numbers, float32(f))
-  }
-  return numbers, nil
+	var numbers []float32
+	for _, v := range x {
+		f, err := strconv.ParseFloat(v, 32)
+		if err != nil {
+			return numbers, err
+		}
+		numbers = append(numbers, float32(f))
+	}
+	return numbers, nil
 }
 
 func mean(x []float32) float32 {
-  var total float32 = 0.0
-  for _, v := range x {
-    total += v
-  }
-  return total / float32(len(x))
+	var total float32 = 0.0
+	for _, v := range x {
+		total += v
+	}
+	return total / float32(len(x))
 }
 
 func lineToProto(
-    line []string, deviceID string) (*measurement.Measurement, error) {
-  if len(line) < 2 {
-    return nil, errors.New(
-        "Line length must be at least 2 (timestamp and one measurement)")
-  }
+	line []string, deviceID string) (*measurement.Measurement, error) {
+	if len(line) < 2 {
+		return nil, errors.New(
+			"Line length must be at least 2 (timestamp and one measurement)")
+	}
 
-  // Convert the timestamp string to a timestamp for packing into a protobuf
-  timestamp, err := time.Parse(timeFormat, line[0])
-  if err != nil {
-    return nil, err
-  }
-  timestampProto, err := ptypes.TimestampProto(timestamp)
-  if err != nil {
-    return nil, err
-  }
+	// Convert the timestamp string to a timestamp for packing into a protobuf
+	timestamp, err := time.Parse(timeFormat, line[0])
+	if err != nil {
+		return nil, err
+	}
+	timestampProto, err := ptypes.TimestampProto(timestamp)
+	if err != nil {
+		return nil, err
+	}
 
-  temps, err := strsToFloats(line[1:])
-  if err != nil {
-    return nil, err
-  }
+	temps, err := strsToFloats(line[1:])
+	if err != nil {
+		return nil, err
+	}
 
-  m := &measurement.Measurement{
-    DeviceId: deviceID,
-    Timestamp: timestampProto,
-    Temp: mean(temps),
-  }
+	m := &measurement.Measurement{
+		DeviceId:  deviceID,
+		Timestamp: timestampProto,
+		Temp:      mean(temps),
+	}
 
-  return m, nil
+	return m, nil
 }
 
 func publishFromCSV(csvFile string, projectID string, pubsubTopic string,
-                    deviceID string) error {
-  f, err := os.Open(csvFile)
-  defer f.Close()
-  if err != nil {
-    return err
-  }
-  reader := csv.NewReader(bufio.NewReader(f))
+	deviceID string) error {
+	f, err := os.Open(csvFile)
+	defer f.Close()
+	if err != nil {
+		return err
+	}
+	reader := csv.NewReader(bufio.NewReader(f))
 
-  // Set up the Pub/Sub client
-  ctx := context.Background()
-  client, err := pubsub.NewClient(ctx, projectID)
-  if err != nil {
-    return err
-  }
+	// Set up the Pub/Sub client
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, projectID)
+	if err != nil {
+		return err
+	}
 
-  if exists, err := client.Topic(pubsubTopic).Exists(ctx); err != nil {
-    return err
-  } else if !exists {
-    return errors.New(
-        fmt.Sprintf("Topic does not exist. Create it first: %v", pubsubTopic))
-  }
+	if exists, err := client.Topic(pubsubTopic).Exists(ctx); err != nil {
+		return err
+	} else if !exists {
+		return errors.New(
+			fmt.Sprintf("Topic does not exist. Create it first: %v", pubsubTopic))
+	}
 
-  topic := client.Topic(pubsubTopic)
-  defer topic.Stop()
+	topic := client.Topic(pubsubTopic)
+	defer topic.Stop()
 
-  topic.PublishSettings = pubsub.PublishSettings{
-    DelayThreshold: 10 * time.Millisecond,
-    CountThreshold: 50,
-  }
+	topic.PublishSettings = pubsub.PublishSettings{
+		DelayThreshold: 10 * time.Millisecond,
+		CountThreshold: 50,
+	}
 
-  // Assume that the CSV file has column headers, and strip them off first
-  if _, err := reader.Read(); err == io.EOF {
-    return err
-  }
+	// Assume that the CSV file has column headers, and strip them off first
+	if _, err := reader.Read(); err == io.EOF {
+		return err
+	}
 
-  for {
-    line, err := reader.Read()
-    if err == io.EOF {
-      break
-    } else if err != nil {
-      return err
-    }
+	for {
+		line, err := reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
 
-    // Make and marshal the protobuf
-    m, err := lineToProto(line, deviceID)
-    if err != nil {
-      return err
-    }
-    data, err := proto.Marshal(m)
-    if err != nil {
-      return err
-    }
+		// Make and marshal the protobuf
+		m, err := lineToProto(line, deviceID)
+		if err != nil {
+			return err
+		}
+		data, err := proto.Marshal(m)
+		if err != nil {
+			return err
+		}
 
-    topic.Publish(ctx, &pubsub.Message{Data: data})
-  }
+		topic.Publish(ctx, &pubsub.Message{Data: data})
+	}
 
-  return nil
+	return nil
 }
 
 func usage() {
-  fmt.Printf(usageStr, path.Base(os.Args[0]), timeFormat)
+	fmt.Printf(usageStr, path.Base(os.Args[0]), timeFormat)
 }
 
 func main() {
-  if len(os.Args) != 5 {
-    usage()
-    os.Exit(2)
-  }
+	if len(os.Args) != 5 {
+		usage()
+		os.Exit(2)
+	}
 
-  err := publishFromCSV(os.Args[1], os.Args[2], os.Args[3], os.Args[4])
-  if err != nil {
-    fmt.Println(err)
-    os.Exit(1)
-  }
+	err := publishFromCSV(os.Args[1], os.Args[2], os.Args[3], os.Args[4])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
--- scripts/main_test.go
+++ scripts/main_test.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-  "math"
-  "testing"
+	"math"
+	"testing"
 )
 
 const epsilon = 0.00001
+
 func floatsEqual(a float32, b float32) bool {
-  return math.Abs(float64(a - b)) < epsilon
+	return math.Abs(float64(a-b)) < epsilon
 }
 
 /*
@@ -15,39 +16,39 @@ func floatsEqual(a float32, b float32) bool {
  */
 
 func TestStrsToFloatsEmpty(t *testing.T) {
-  floats, err := strsToFloats([]string{})
-  if err != nil {
-    t.Errorf("Error on empty list: %v", err)
-  }
+	floats, err := strsToFloats([]string{})
+	if err != nil {
+		t.Errorf("Error on empty list: %v", err)
+	}
 
-  if len(floats) != 0 {
-    t.Errorf("Result list has len %v, expected it to be empty", len(floats))
-  }
+	if len(floats) != 0 {
+		t.Errorf("Result list has len %v, expected it to be empty", len(floats))
+	}
 }
 
 func TestStrsToFloatsValid(t *testing.T) {
-  input := []string{"10.0", "-3.9", "0.03"}
-  output := []float32{10.0, -3.9, 0.03}
+	input := []string{"10.0", "-3.9", "0.03"}
+	output := []float32{10.0, -3.9, 0.03}
 
-  floats, err := strsToFloats(input)
-  if err != nil {
-    t.Errorf("Error on valid input: %v", err)
-  }
+	floats, err := strsToFloats(input)
+	if err != nil {
+		t.Errorf("Error on valid input: %v", err)
+	}
 
-  for i, _ := range input {
-    if !floatsEqual(floats[i], output[i]) {
-      t.Errorf("Incorrect for input %q: Expected %v, got %v",
-               input[i], output[i], floats[i])
-    }
-  }
+	for i, _ := range input {
+		if !floatsEqual(floats[i], output[i]) {
+			t.Errorf("Incorrect for input %q: Expected %v, got %v",
+				input[i], output[i], floats[i])
+		}
+	}
 }
 
 func TestStrsToFloatsInvalid(t *testing.T) {
-  _, err := strsToFloats([]string{"5.0", "spam", "spam", "spam",
-                                  "baked beans", "spam"})
-  if err == nil {
-    t.Error("Expected error on invalid input, but error is nil")
-  }
+	_, err := strsToFloats([]string{"5.0", "spam", "spam", "spam",
+		"baked beans", "spam"})
+	if err == nil {
+		t.Error("Expected error on invalid input, but error is nil")
+	}
 }
 
 /*
@@ -55,24 +56,24 @@ func TestStrsToFloatsInvalid(t *testing.T) {
  */
 
 func TestMeanEmpty(t *testing.T) {
-  m := mean([]float32{})
-  if !math.IsNaN(float64(m)) {
-    t.Errorf("Expected NaN, got %v", m)
-  }
+	m := mean([]float32{})
+	if !math.IsNaN(float64(m)) {
+		t.Errorf("Expected NaN, got %v", m)
+	}
 }
 
 func TestMeanSingle(t *testing.T) {
-  m := mean([]float32{12.7})
-  if !floatsEqual(m, 12.7) {
-    t.Errorf("Expected 12.7, got %v", m)
-  }
+	m := mean([]float32{12.7})
+	if !floatsEqual(m, 12.7) {
+		t.Errorf("Expected 12.7, got %v", m)
+	}
 }
 
 func TestMeanMultiple(t *testing.T) {
-  m := mean([]float32{10.0, 20.0, 42.6})
-  if !floatsEqual(m, 24.2) {
-    t.Errorf("Expected 15.0, got %v", m)
-  }
+	m := mean([]float32{10.0, 20.0, 42.6})
+	if !floatsEqual(m, 24.2) {
+		t.Errorf("Expected 15.0, got %v", m)
+	}
 }
 
 /*
@@ -80,32 +81,32 @@ func TestMeanMultiple(t *testing.T) {
  */
 
 func TestLineToProtoEmpty(t *testing.T) {
-  _, err := lineToProto([]string{}, "foo")
-  if err == nil {
-    t.Error("Expected error on invalid input, but error is nil")
-  }
+	_, err := lineToProto([]string{}, "foo")
+	if err == nil {
+		t.Error("Expected error on invalid input, but error is nil")
+	}
 }
 
 func TestLineToProtoNoMeasurements(t *testing.T) {
-  _, err := lineToProto([]string{"2006-01-02T15:04:05.999999"}, "foo")
-  if err == nil {
-    t.Error("Expected error on invalid input, but error is nil")
-  }
+	_, err := lineToProto([]string{"2006-01-02T15:04:05.999999"}, "foo")
+	if err == nil {
+		t.Error("Expected error on invalid input, but error is nil")
+	}
 }
 
 func TestLineToProtoValid(t *testing.T) {
-  deviceID := "foo"
-  m, err := lineToProto([]string{
-      "2006-01-02T15:04:05.999999", "18.5", "18.0", "18.6"}, deviceID)
-  if err != nil {
-    t.Errorf("Failed to convert line: %v", err)
-  }
+	deviceID := "foo"
+	m, err := lineToProto([]string{
+		"2006-01-02T15:04:05.999999", "18.5", "18.0", "18.6"}, deviceID)
+	if err != nil {
+		t.Errorf("Failed to convert line: %v", err)
+	}
 
-  if m.GetDeviceId() != deviceID {
-    t.Errorf("Device ID Expected to be %q, got %q", deviceID, m.GetDeviceId())
-  }
+	if m.GetDeviceId() != deviceID {
+		t.Errorf("Device ID Expected to be %q, got %q", deviceID, m.GetDeviceId())
+	}
 
-  if !floatsEqual(m.GetTemp(), 18.366667) {
-    t.Errorf("Expected 18.366667, got %v", m.GetTemp())
-  }
+	if !floatsEqual(m.GetTemp(), 18.366667) {
+		t.Errorf("Expected 18.366667, got %v", m.GetTemp())
+	}
 }

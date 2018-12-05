d46726f3f230b6652dd2ef5367ff2b2da7ec207c
Michael Traver <michael.r.traver@gmail.com>
2018-12-04 18:18:35 -0800

    Re-generate Python protobuf code using protoc 3.6.1
    Previous code was generated with protoc 3.5.1.

--- client_python/loggers/measurement_pb2.py
+++ client_python/loggers/measurement_pb2.py
@@ -7,7 +7,6 @@ from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
 from google.protobuf import reflection as _reflection
 from google.protobuf import symbol_database as _symbol_database
-from google.protobuf import descriptor_pb2
 # @@protoc_insertion_point(imports)
 
 _sym_db = _symbol_database.Default()
@@ -21,6 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   name='measurement.proto',
   package='measurement',
   syntax='proto3',
+  serialized_options=_b('Z\013measurement'),
   serialized_pb=_b('\n\x11measurement.proto\x12\x0bmeasurement\x1a google/protobuf/descriptor.proto\x1a\x1fgoogle/protobuf/timestamp.proto\"\x7f\n\x0bMeasurement\x12\x33\n\tdevice_id\x18\x01 \x01(\tB \x82\xb5\x18\x1c^[a-z][a-z0-9+.%~_-]{2,254}$\x12-\n\ttimestamp\x18\x02 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12\x0c\n\x04temp\x18\x03 \x01(\x02:.\n\x05regex\x12\x1d.google.protobuf.FieldOptions\x18\xd0\x86\x03 \x01(\tB\rZ\x0bmeasurementb\x06proto3')
   ,
   dependencies=[google_dot_protobuf_dot_descriptor__pb2.DESCRIPTOR,google_dot_protobuf_dot_timestamp__pb2.DESCRIPTOR,])
@@ -33,7 +33,7 @@ regex = _descriptor.FieldDescriptor(
   has_default_value=False, default_value=_b("").decode('utf-8'),
   message_type=None, enum_type=None, containing_type=None,
   is_extension=True, extension_scope=None,
-  options=None, file=DESCRIPTOR)
+  serialized_options=None, file=DESCRIPTOR)
 
 
 _MEASUREMENT = _descriptor.Descriptor(
@@ -49,28 +49,28 @@ _MEASUREMENT = _descriptor.Descriptor(
       has_default_value=False, default_value=_b("").decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
-      options=_descriptor._ParseOptions(descriptor_pb2.FieldOptions(), _b('\202\265\030\034^[a-z][a-z0-9+.%~_-]{2,254}$')), file=DESCRIPTOR),
+      serialized_options=_b('\202\265\030\034^[a-z][a-z0-9+.%~_-]{2,254}$'), file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
       name='timestamp', full_name='measurement.Measurement.timestamp', index=1,
       number=2, type=11, cpp_type=10, label=1,
       has_default_value=False, default_value=None,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
-      options=None, file=DESCRIPTOR),
+      serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
       name='temp', full_name='measurement.Measurement.temp', index=2,
       number=3, type=2, cpp_type=6, label=1,
       has_default_value=False, default_value=float(0),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
-      options=None, file=DESCRIPTOR),
+      serialized_options=None, file=DESCRIPTOR),
   ],
   extensions=[
   ],
   nested_types=[],
   enum_types=[
   ],
-  options=None,
+  serialized_options=None,
   is_extendable=False,
   syntax='proto3',
   extension_ranges=[],
@@ -94,8 +94,6 @@ _sym_db.RegisterMessage(Measurement)
 
 google_dot_protobuf_dot_descriptor__pb2.FieldOptions.RegisterExtension(regex)
 
-DESCRIPTOR.has_options = True
-DESCRIPTOR._options = _descriptor._ParseOptions(descriptor_pb2.FileOptions(), _b('Z\013measurement'))
-_MEASUREMENT.fields_by_name['device_id'].has_options = True
-_MEASUREMENT.fields_by_name['device_id']._options = _descriptor._ParseOptions(descriptor_pb2.FieldOptions(), _b('\202\265\030\034^[a-z][a-z0-9+.%~_-]{2,254}$'))
+DESCRIPTOR._options = None
+_MEASUREMENT.fields_by_name['device_id']._options = None
 # @@protoc_insertion_point(module_scope)

0016f243dfa4fd84c93b5aa339b12ed4aa10d150
Michael Traver <michael.r.traver@gmail.com>
2019-03-25 22:55:06 -0700

    readtemp cmd: add flags for JSON and binary protobuf output

--- cmd/readtemp/main.go
+++ cmd/readtemp/main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"time"
 
+	"github.com/golang/protobuf/jsonpb"
+	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/ptypes"
 	"github.com/mtraver/mcp9808"
+
+	measurementpb "github.com/mtraver/environmental-sensor/measurement"
+)
+
+var (
+	jsonOut   bool
+	binaryOut bool
 )
 
 func fatalf(format string, a ...interface{}) {
@@ -12,7 +24,62 @@ func fatalf(format string, a ...interface{}) {
 	os.Exit(1)
 }
 
+func init() {
+	flag.BoolVar(&jsonOut, "json", false, "output a JSON-encoded proto instead of plain temp value")
+	flag.BoolVar(&binaryOut, "binary", false, "output a binary-encoded proto instead of plain temp value")
+}
+
+func parseFlags() error {
+	flag.Parse()
+
+	if jsonOut && binaryOut {
+		return fmt.Errorf("only one of -json and -binary may be given")
+	}
+
+	return nil
+}
+
+func makeProto(temp float32) (*measurementpb.Measurement, error) {
+	timepb, err := ptypes.TimestampProto(time.Now().UTC())
+	if err != nil {
+		return nil, err
+	}
+
+	return &measurementpb.Measurement{
+		DeviceId:  "none",
+		Timestamp: timepb,
+		Temp:      temp,
+	}, nil
+}
+
+func toJSONProto(temp float32) (string, error) {
+	m, err := makeProto(temp)
+	if err != nil {
+		return "", err
+	}
+
+	marshaler := jsonpb.Marshaler{
+		Indent: "  ",
+	}
+	return marshaler.MarshalToString(m)
+}
+
+func toBinaryProto(temp float32) ([]byte, error) {
+	m, err := makeProto(temp)
+	if err != nil {
+		return nil, err
+	}
+
+	return proto.Marshal(m)
+}
+
 func main() {
+	if err := parseFlags(); err != nil {
+		fmt.Printf("argument error: %v\n", err)
+		flag.Usage()
+		os.Exit(2)
+	}
+
 	sensor, err := mcp9808.NewMCP9808()
 	if err != nil {
 		fatalf("Error connecting to sensor: %v", err)
@@ -28,5 +95,21 @@ func main() {
 		fatalf("Failed to read temp: %v", err)
 	}
 
-	fmt.Println(temp)
+	if jsonOut {
+		json, err := toJSONProto(temp)
+		if err != nil {
+			fatalf("Failed to make JSON proto: %v", err)
+		}
+
+		fmt.Println(json)
+	} else if binaryOut {
+		pbBytes, err := toBinaryProto(temp)
+		if err != nil {
+			fatalf("Failed to make binary proto: %v", err)
+		}
+
+		fmt.Print(pbBytes)
+	} else {
+		fmt.Println(temp)
+	}
 }

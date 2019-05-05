649ed80453491af698e6ca3a3a5a3e7c68e37a72
Michael Traver <michael.r.traver@gmail.com>
2019-05-05 13:48:42 -0700

    Move func pushHandler into its own file

--- web/main.go
+++ web/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"html/template"
 	"log"
@@ -11,7 +10,6 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/golang/protobuf/proto"
 	"github.com/mtraver/gaelog"
 	"google.golang.org/appengine"
 
@@ -59,17 +57,6 @@ func mustGetenv(varName string) string {
 		log.Fatalf("Environment variable must be set: %v\n", varName)
 	}
 	return val
-}
-
-// This is the structure of the JSON payload pushed to the endpoint by
-// Cloud Pub/Sub. See https://cloud.google.com/pubsub/docs/push.
-type pushRequest struct {
-	Message struct {
-		Attributes map[string]string
-		Data       []byte
-		ID         string `json:"message_id"`
-	}
-	Subscription string
 }
 
 func init() {
@@ -214,49 +201,4 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	if err := templates.ExecuteTemplate(w, "index", data); err != nil {
 		lg.Errorf("Could not execute template: %v", err)
 	}
-}
-
-func pushHandler(w http.ResponseWriter, r *http.Request) {
-	ctx := appengine.NewContext(r)
-
-	lg, err := gaelog.New(r)
-	if err != nil {
-		lg.Errorf("%v", err)
-	}
-	defer lg.Close()
-
-	msg := &pushRequest{}
-	if err := json.NewDecoder(r.Body).Decode(msg); err != nil {
-		lg.Criticalf("Could not decode body: %v\n", err)
-		http.Error(w, fmt.Sprintf("Could not decode body: %v", err),
-			http.StatusBadRequest)
-		return
-	}
-
-	m := &measurement.Measurement{}
-	if err := proto.Unmarshal(msg.Message.Data, m); err != nil {
-		lg.Criticalf("Failed to unmarshal protobuf: %v\n", err)
-		http.Error(w, fmt.Sprintf("Failed to unmarshal protobuf: %v", err),
-			http.StatusBadRequest)
-		return
-	}
-
-	if err := m.Validate(); err != nil {
-		lg.Errorf("%v", err)
-
-		// Pub/Sub will only stop re-trying the message if it receives a status 200.
-		// The docs say that any of 200, 201, 202, 204, or 102 will have this effect
-		// (https://cloud.google.com/pubsub/docs/push), but the local emulator
-		// doesn't respect anything other than 200, so return 200 just to be safe.
-		// TODO(mtraver) I'd rather return e.g. 202 (http.StatusAccepted) to
-		// indicate that it was successfully received but not that all is ok.
-		w.WriteHeader(http.StatusOK)
-		return
-	}
-
-	if err := database.Save(ctx, m); err != nil {
-		lg.Errorf("Failed to save measurement: %v\n", err)
-	}
-
-	w.WriteHeader(http.StatusOK)
 }
---
+++ web/push.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/mtraver/gaelog"
+	"google.golang.org/appengine"
+
+	"github.com/mtraver/environmental-sensor/measurement"
+)
+
+// This is the structure of the JSON payload pushed to the endpoint by Cloud Pub/Sub.
+// See https://cloud.google.com/pubsub/docs/push.
+type pushRequest struct {
+	Message struct {
+		Attributes map[string]string
+		Data       []byte
+		ID         string `json:"message_id"`
+	}
+	Subscription string
+}
+
+func pushHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+
+	lg, err := gaelog.New(r)
+	if err != nil {
+		lg.Errorf("%v", err)
+	}
+	defer lg.Close()
+
+	msg := &pushRequest{}
+	if err := json.NewDecoder(r.Body).Decode(msg); err != nil {
+		lg.Criticalf("Could not decode body: %v\n", err)
+		http.Error(w, fmt.Sprintf("Could not decode body: %v", err),
+			http.StatusBadRequest)
+		return
+	}
+
+	m := &measurement.Measurement{}
+	if err := proto.Unmarshal(msg.Message.Data, m); err != nil {
+		lg.Criticalf("Failed to unmarshal protobuf: %v\n", err)
+		http.Error(w, fmt.Sprintf("Failed to unmarshal protobuf: %v", err),
+			http.StatusBadRequest)
+		return
+	}
+
+	if err := m.Validate(); err != nil {
+		lg.Errorf("%v", err)
+
+		// Pub/Sub will only stop re-trying the message if it receives a status 200.
+		// The docs say that any of 200, 201, 202, 204, or 102 will have this effect
+		// (https://cloud.google.com/pubsub/docs/push), but the local emulator
+		// doesn't respect anything other than 200, so return 200 just to be safe.
+		// TODO(mtraver) I'd rather return e.g. 202 (http.StatusAccepted) to
+		// indicate that it was successfully received but not that all is ok.
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	if err := database.Save(ctx, m); err != nil {
+		lg.Errorf("Failed to save measurement: %v\n", err)
+	}
+
+	w.WriteHeader(http.StatusOK)
+}

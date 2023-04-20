578fe85127095eab33afdcb15fb079100c349864
Michael Traver <michael.r.traver@gmail.com>
2023-04-19 22:48:18 -0700

    Set a temperature in the dummy sensor

--- sensor/dummy/dummy.go
+++ sensor/dummy/dummy.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	mpb "github.com/mtraver/environmental-sensor/measurementpb"
+	wpb "google.golang.org/protobuf/types/known/wrapperspb"
 )
 
 type Dummy struct{}
@@ -15,6 +16,7 @@ func (d Dummy) Init() error {
 
 func (d Dummy) Sense(m *mpb.Measurement) error {
 	log.Printf("DUMMY SENSOR SENSE")
+	m.Temp = wpb.Float(20)
 	return nil
 }
 

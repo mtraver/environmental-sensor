05722dab97fa13c580a6001824d2cfac96027c33
Michael Traver <michael.r.traver@gmail.com>
2026-07-23 12:18:44 -0700

    Printf -> Println for prints with no formatting

--- sensor/dummy/dummy.go
+++ sensor/dummy/dummy.go
@@ -11,32 +11,32 @@ import (
 type Dummy struct{}
 
 func (d Dummy) OnRegister() error {
-	log.Printf("DUMMY SENSOR OnRegister")
+	log.Println("DUMMY SENSOR OnRegister")
 	return nil
 }
 
 func (d Dummy) OnRemove() error {
-	log.Printf("DUMMY SENSOR OnRemove")
+	log.Println("DUMMY SENSOR OnRemove")
 	return nil
 }
 
 func (s Dummy) Configure(raw json.RawMessage) error {
-	log.Printf("DUMMY SENSOR Configure")
+	log.Println("DUMMY SENSOR Configure")
 	return nil
 }
 
 func (d Dummy) RunSetupJob() error {
-	log.Printf("DUMMY SENSOR RunSetupJob")
+	log.Println("DUMMY SENSOR RunSetupJob")
 	return nil
 }
 
 func (d Dummy) RunSenseJob(m *mpb.Measurement) error {
-	log.Printf("DUMMY SENSOR RunSenseJob")
+	log.Println("DUMMY SENSOR RunSenseJob")
 	m.Temp = wpb.Float(20)
 	return nil
 }
 
 func (d Dummy) RunShutdownJob() error {
-	log.Printf("DUMMY SENSOR RunShutdownJob")
+	log.Println("DUMMY SENSOR RunShutdownJob")
 	return nil
 }
